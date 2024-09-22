1a9be53ace6ac585f243c9bf0dad635d94fa624a
kaleycode <readingkale@gmail.com>
2024-09-22 11:49:17 -0700

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} Books For You
+            </p>
+        </footer>
+    );
+}
+export default Footer 
---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+
+function Header(){
+    return(
+        <header>
+            <h1>Books</h1>
+            <hr></hr>
+            <nav>
+                <ul>
+                    <li>< a href = "">Home</a></li>
+                    <li><a href = "">Popular</a></li>
+                    <li><a href = "">Classics</a></li>
+                    <li><a href = "">Sci-Fi / Fantasy</a></li>
+                    <li><a href = "">Children's</a></li>
+                    <li><a href="">Nonfiction</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+export default Header
